d677de1d33c6dd84cdf901fff6500f8ac7edfaa5
PavloVeretilnyi <pashtet9112@gmail.com>
2025-08-27 15:47:02 +0200

    From the local repo

--- final_project/router/booksdb.js
+++ final_project/router/booksdb.js
@@ -1,14 +1,14 @@
 let books = {
-      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {} },
-      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {} },
-      3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {} },
-      4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {} },
-      5: {"author": "Unknown","title": "The Book Of Job", "reviews": {} },
-      6: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {} },
-      7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {} },
-      8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {} },
-      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {} },
-      10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {} }
+      "0385474547": {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {} },
+      "3836526751": {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {} },
+      "0451208633": {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {} },
+      "0848805011": {"author": "Sandars","title": "The Epic Of Gilgamesh", "reviews": {} },
+      "0060969598": {"author": "Stephen Mitchell","title": "The Book Of Job", "reviews": {} },
+      "0307948994": {"author": "Hanan al-Shaykh","title": "One Thousand and One Nights", "reviews": {} },
+      "0140441034": {"author": "Hermann Palsson","title": "Njal's Saga", "reviews": {} },
+      "0141439513": {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {} },
+      "0192835696": {"author": "Honore de Balzac","title": "Le Pere Goriot", "reviews": {} },
+      "0330256645": {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {} }
 }
 
 module.exports=books;
