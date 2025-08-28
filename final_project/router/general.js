c2d5903a3647ec73e06acbe974846820a98af2f0
PavloVeretilnyi <pashtet9112@gmail.com>
2025-08-28 07:07:31 -0400

    Promise based implementation

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -25,32 +25,106 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  res.send(JSON.stringify(books,null,4));
+
+    //res.send(JSON.stringify(books,null,4));
+
+    //same functionality but with Promise based implementation:
+    const getBooks = new Promise((resolve, reject) => {
+        if (books) {
+        resolve(books);   // Resolve with the books object
+        } else {
+        reject("No books available");
+        }
+    });
+    
+    getBooks
+    .then(result => {
+      res.send(JSON.stringify(result, null, 4));
+    })
+    .catch(error => {
+      res.status(500).json({ message: error });
+    });
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-    // Retrieve the email parameter from the request URL and send the corresponding books's details
+    // Retrieve the ISBN parameter from the request URL and send the corresponding books's details
+    /*const isbn = req.params.isbn;
+    res.send(books[isbn])*/
+    //same functionality but with Promise based implementation
     const isbn = req.params.isbn;
-    res.send(books[isbn])
+    const getBookByISBN = new Promise((resolve, reject) => {
+      const book = books[isbn];
+      if (book) {
+        resolve(book);
+      } else {
+        reject(`Book with ISBN ${isbn} not found`);
+      }
+    });  
+    
+    getBookByISBN
+      .then(result => {
+        res.send(result);
+      })
+      .catch(error => {
+        res.status(404).json({ message: error });
+      });
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  let author = decodeURIComponent(req.params.author).toLowerCase();
-  let results = Object.values(books).filter(
-    book => book.author.toLowerCase() === author
-  );
-  res.send(results);  
+    let author = decodeURIComponent(req.params.author).toLowerCase();
+    /*let results = Object.values(books).filter(
+        book => book.author.toLowerCase() === author
+    );
+    res.send(results);*/
+    //same functionality but with Promise based implementation:
+    const getBooksByAuthor = new Promise((resolve, reject) => {
+      let results = Object.values(books).filter(
+        book => book.author.toLowerCase() === author
+      );  
+      if (results.length > 0) {
+        resolve(results);
+      } else {
+        reject(`No books found for author: ${author}`);
+      }
+    });
+  
+    getBooksByAuthor
+      .then(results => {
+        res.send(results);
+      })
+      .catch(error => {
+        res.status(404).json({ message: error });
+      });
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
     let title = decodeURIComponent(req.params.title).toLowerCase();
-    let results = Object.values(books).filter(
+    /*let results = Object.values(books).filter(
     book => book.title.toLowerCase() === title
-  );
-  res.send(results);
+    );
+    res.send(results);*/
+    //same functionality but with Promise based implementation:
+    const getBooksByTitle = new Promise((resolve, reject) => {
+        let results = Object.values(books).filter(
+          book => book.title.toLowerCase() === title
+        );
+        if (results.length > 0) {
+          resolve(results);
+        } else {
+          reject(`No books found with title: ${title}`);
+        }
+    });
+    
+    getBooksByTitle
+        .then(results => {
+          res.send(results);
+        })
+        .catch(error => {
+          res.status(404).json({ message: error });
+        });
 });
 
 //  Get book review
